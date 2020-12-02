3b55c00d73a9e8677d3e3ca8a8c780bb329c9a01
Shawn McGraw <shawn.mcgraw@protonmail.com>
2020-12-02 18:19:52 -0500

    validation and url_for

--- app/routes.py
+++ app/routes.py
@@ -19,6 +19,7 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -26,5 +27,5 @@ def login():
         flash('Login requested for uer {}, remember_me={}'.format(
             form.username.data, form.remember_me.data
         ))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
